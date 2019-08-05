2083420ccd273233a2cc57f2829c71e8ea79e381
yangjian.fe <yangjian.fe@bytedance.com>
2019-08-05 10:23:05 +0800

    feat: add version

--- src/cli.ts
+++ src/cli.ts
@@ -1,6 +1,13 @@
 import program from 'commander';
+import path from 'path';
+import fs from 'fs';
+const pkgPath = path.join(__dirname, '../package.json');
+console.log('pkgPath:', pkgPath);
+const pkg = fs.readFileSync(pkgPath).toString();
+const { version } = JSON.parse(pkg);
 
-program.version('0.0.1')
+
+program.version(version)
 .description('Facke package manager')
 .command('install [name]', 'install on or more packages')
 .command('search [query]', 'search with optional query')
